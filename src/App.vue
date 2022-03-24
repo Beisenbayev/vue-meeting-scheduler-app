3b5f6cca0c747cf68569bc518df87602941fd2f0
beisenbayev <beisenbayeff.nursultan@gmail.com>
2022-03-24 08:24:22 +0600

    main init

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <Header title="Task Tracker" />
+</template>
+
+<script setup>
+import Header from './components/Header.vue';
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,12 @@
+<template>
+   <header>
+      <h2>{{ title }}</h2>
+   </header>
+</template>
+
+<script setup>
+const props = defineProps({
+   title: String
+});
+</script>
+
